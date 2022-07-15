26754720277094ad051b77d6db3ed125ce476c4a
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-15 18:06:03 +0100

    Disable Reloader (for now...)

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 
-class Config(object):
+class Config():
     SECRET_KEY = 'make this more secret'
     FLASK_SECRET = SECRET_KEY
     DB_HOST = 'database' # do this later
@@ -13,5 +13,4 @@ class Config(object):
 class DebugConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    DB_HOST = 'my.production.database' 
-
+    DB_HOST = 'my.production.database'
