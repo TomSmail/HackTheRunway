4775466f005a0d888d024d4be8a10535c230a99f
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-15 18:12:25 +0100

    Clean up

--- fashionmatch/home/home.py
+++ fashionmatch/home/home.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, url_for, redirect, request, render_template, flash, session
-# from flask import current_app as app
-# from flask import render_template
 
 
 main_bp = Blueprint(
-    "main_bp", __name__, template_folder="templates", static_folder="static", static_url_path='/mstatic'
+    "main_bp", __name__, template_folder="templates", static_folder="static", static_url_path='/static'
 )
 
 
