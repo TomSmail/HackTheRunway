47519f4f320d898a6661c820b9cbfe8e55c6ebaa
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-16 13:54:01 +0100

    Improve auth & DB

--- fashionmatch/auth.py
+++ fashionmatch/auth.py
@@ -6,7 +6,7 @@ from functools import wraps
 def ensurelogin(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if bool(session.get('email', False)):
-            return session.get('email', False)
-        return redirect(url_for('account_bp.login'))
+        if not bool(session.get('email', False)):
+            return redirect(url_for('account_bp.login'))
+        return f(*args, **kwargs)
     return wrap
