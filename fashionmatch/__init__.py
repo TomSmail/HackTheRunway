c65bd1a7fbcef8fed18ec5c2081f809d3aa5e229
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-16 10:54:25 +0100

    Get rid of sqlalcemy

--- fashionmatch/__init__.py
+++ fashionmatch/__init__.py
@@ -6,11 +6,12 @@ from .swap import swap
 
 import config
 
-#db = SQLAlchemy()
+
 
 def init_app():
     """Create Flask application."""
-    app = Flask(__name__, instance_relative_config=False, template_folder="templates", static_folder="static")
+    app = Flask(__name__, instance_relative_config=False,
+                template_folder="templates", static_folder="static")
 
     with app.app_context():
 
@@ -21,5 +22,5 @@ def init_app():
         app.register_blueprint(home.home_bp)
         app.register_blueprint(account.account_bp, url_prefix="/account")
         app.register_blueprint(swap.swap_bp, url_prefix="/swap")
-        
+
         return app
