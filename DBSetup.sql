6d06f91a966a7721c7f18b50720d348655a102ad
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-07-16 10:12:55 +0100

    Created Database according to attached schema

--- DBSetup.sql
+++ DBSetup.sql
@@ -1,44 +1,38 @@
-CREATE TABLE User(
-	UserID INT AUTOINCREMENT PRIMARY_KEY,
+CREATE TABLE "User" (
+	UserID SERIAL PRIMARY KEY NOT NULL,
 	Email VARCHAR(100),
 	PasswordHash VARCHAR(512),
 	Address TEXT,
 	ProfilePictureLink Text
 );
 
-CREATE TABLE Article(
-	ArticleID INT AUTOINCREMENT PRIMARY_KEY,
+CREATE TABLE "Article" (
+	ArticleID SERIAL PRIMARY KEY,
 	Color VARCHAR(30),
 	TypeOfClothing VARCHAR(30),
 	PriceRange INT, --categorised to discrete integers
-	Condition INT, --categorised to discrete integers
+	Condition INT --categorised to discrete integers
 );
 
-CREATE TABLE User_Has(
-	HasID INT AUTOINCREMENT PRIMARY_KEY,
-	HasUserID INT,
-	ArticleID INT,
-	FOREIGN KEY HasUserID REFERENCES Users(UserID),
-	FOREIGN KEY ArticleID REFERENCES Articles(ArticleID)
+CREATE TABLE "User_Has" (
+	HasID SERIAL PRIMARY KEY,
+	HasUserID INT REFERENCES "User"(UserID),
+	ArticleID INT REFERENCES "Article"(ArticleID)
 );
 
-CREATE TABLE User_Wants(
-	WantsID INT AUTOINCREMENT PRIMARY_KEY,
-	WantsUserID INT,
-	ArticleID INT,
-	FOREIGN KEY WantsUserID REFERENCES Users(UserID),
-	FOREIGN KEY ArticleID REFERENCES Articles(ArticleID)
+CREATE TABLE "User_Wants" (
+	WantsID SERIAL PRIMARY KEY,
+	WantsUserID INT REFERENCES "User"(UserID),
+	ArticleID INT REFERENCES "Article"(ArticleID)
 );
 
-CREATE TABLE MatchArticle(
+CREATE TABLE "Match_Article" (
+	MatchArticleID SERIAL PRIMARY KEY,
 	MatchID INT,
-	HasID INT,
-	WantsID INT,
-	Status VARCHAR(10)
-	FOREIGN KEY HasID REFERENCES User_Has(HasID),
-	FOREIGN KEY WantsID REFERENCES User_Wants(WantsID)
+	HasID INT REFERENCES "User_Has"(HasID),
+	WantsID INT REFERENCES "User_Wants"(WantsID),
+	Status VARCHAR(10) 
 );
 
+SELECT * FROM information_schema.tables WHERE table_schema = 'public';
 
-#ActiveMatches(MatchID,Status)
-MatchArticle(MatchID,HasID,WantsID,Status) #Status Agreed by both parties etc.
