7fe2479b06c6de21639ae32b384dd9c01597dd22
Quentin Duff <30555853+qduff@users.noreply.github.com>
2022-07-17 11:40:50 +0100

    Add user Wants on home

--- fashionmatch/home/home.py
+++ fashionmatch/home/home.py
@@ -15,18 +15,23 @@ def home():
     # Fetch users items
     db, cur = get_db()
     cur.execute('SELECT typeofclothing, color, pricerange, condition, locationmade, cotton, imageofitem FROM ("Article" INNER JOIN "User_Has" ON "Article".articleid ="User_Has".articleid) INNER JOIN "User" ON "User_Has".hasuserid="User".userid WHERE "User".email=%s;', (session.get("email", None),))
-    resp = cur.fetchall()
-    print(resp)
+    has = cur.fetchall()
 
+    cur.execute('SELECT typeofclothing, color, pricerange, condition FROM ("Article" INNER JOIN "User_Wants" ON "Article".articleid ="User_Wants".articleid) INNER JOIN "User" ON "User_Wants".wantsuserid="User".userid WHERE "User".email=%s;', (session.get("email", None),))
+    wants = cur.fetchall()
+
+    print(wants)
     #Get num of pending swaps
     uid = (session.get("uid", None))
-    print(uid)
+
+
     cur.execute('SELECT * FROM "Match_Article" INNER JOIN "User_Has" ON "Match_Article".HasID="User_Has".HasID WHERE HasUserID=%s;', (str(uid),))
     amount = len(cur.fetchall())
 
     return render_template(
         "home.jinja2",
-        response=resp,
+        response=has,
+        wants=wants,
         amount=amount
     )
 
